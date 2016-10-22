b82564a005648db5fbf72b2f5e93276180990767
wedgwoodtom <wedgwood.tom@gmail.com>
2016-10-22 00:37:54 -0700

    reformat code

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,44 +5,52 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://spring.io/guides/gs/accessing-data-mongodb/
+ */
+
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application implements CommandLineRunner
+{
 
-	@Autowired
-	private CustomerRepository repository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception
+    {
+        customerRepository.deleteAll();
 
-		repository.deleteAll();
+        // save a couple of customers
+        customerRepository.save(new Customer("Alice", "Smith"));
+        customerRepository.save(new Customer("Bob", "Smith"));
 
-		// save a couple of customers
-		repository.save(new Customer("Alice", "Smith"));
-		repository.save(new Customer("Bob", "Smith"));
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : customerRepository.findAll())
+        {
+            System.out.println(customer);
+        }
+        System.out.println();
 
-		// fetch all customers
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (Customer customer : repository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println();
+        // fetch an individual customer
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(customerRepository.findByFirstName("Alice"));
 
-		// fetch an individual customer
-		System.out.println("Customer found with findByFirstName('Alice'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByFirstName("Alice"));
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Customer customer : customerRepository.findByLastName("Smith"))
+        {
+            System.out.println(customer);
+        }
 
-		System.out.println("Customers found with findByLastName('Smith'):");
-		System.out.println("--------------------------------");
-		for (Customer customer : repository.findByLastName("Smith")) {
-			System.out.println(customer);
-		}
-
-	}
+    }
 
 }
