f794b8ac1486dfffc1af63d05fe3f41c596ebf00
wedgwoodtom <wedgwood.tom@gmail.com>
2016-10-24 09:09:27 -0700

    reformat code

--- src/main/java/hello/CustomerRepository.java
+++ src/main/java/hello/CustomerRepository.java
@@ -18,6 +18,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * In a typical Java application, you write a class that implements CustomerRepository and craft
  * the queries yourself. What makes Spring Data MongoDB so useful is the fact that you don’t have
  * to create this implementation. Spring Data MongoDB creates it on the fly when you run the application.
+ *
+ *
+ *
+ * TODO: This seems too abstract to me, at least play with some examples that show more of the magic.
  */
 
 public interface CustomerRepository extends MongoRepository<Customer, String>
